254f4f3b9a43d27cbda25c83fd06f70861cb8436
MinA20041 <minamohammed8282@gmail.com>
2025-01-04 16:15:33 +0200

    commit

---
+++ HW5.C
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main(){ 
+int i=0;
+for(i;i<10;i++) {
+    printf ("%d",i);
+if 
+    (i%2==0) {
+    printf ("even:\n");}
+
+else if
+    (i%2!=0){
+    printf("odd:\n");}
+}
+return(0);
+}
