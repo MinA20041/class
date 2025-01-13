be925742ce1798dd6b54ed760c223fe2802022a3
MinA20041 <minamohammed8282@gmail.com>
2025-01-13 14:00:12 +0200

    initial commit

---
+++ hw1.py
@@ -0,0 +1,13 @@
+
+x = 5
+if x > 0:
+    print("x is positive")
+for i in range(5):
+    print(i)
+y = 10
+if y < 5:
+    print("y is less than 5")
+elif 5 <= y <= 15:
+    print("y is between 5 and 15")
+else:
+    print("y is greater than 15")
